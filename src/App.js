54935cb39253c39afe7cb1f04a1eb3e11621c5ca
keza681 <lindakeza19@gmail.com>
2022-04-02 20:37:56 +0200

    Add components and update Readmefile

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Books from './components/Books';
+import Categories from './components/Categories';
+
+const books = [
+  {
+    id: 1,
+    genre: 'genre',
+    title: 'title',
+    author: 'author',
+    chapter: 'React redux',
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-   
+    <div>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Books books={books} />} />
+        <Route exact path="/categories" element={<Categories />} />
+      </Routes>
     </div>
   );
 }
