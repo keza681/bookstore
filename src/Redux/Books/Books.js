da9d1057596bafeb6b8a5acd09fef929b254b082
keza681 <lindakeza19@gmail.com>
2022-04-08 12:07:16 +0200

    Get data from api

--- src/Redux/Books/Books.js
+++ src/Redux/Books/Books.js
@@ -1,26 +1,72 @@
 const ADD_BOOK = 'ADD_BOOK';
 const REMOVE_BOOK = 'REMOVE_BOOK';
+const UPDATE_BK = 'UPDATE_BK';
+const API = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/dVhWQ90GJqKR2YkPiOuz/books';
 
-export const addBook = (book) => ({ type: ADD_BOOK, payload: book });
-
-export const removeBook = (id) => ({ type: REMOVE_BOOK, payload: id });
-
-const initialState = [{
-  id: 1,
-  genre: 'Lecture',
-  title: 'React',
-  author: 'Marck Bochelier',
-  chapter: 'programming',
-}];
+const initialState = [];
 
 const booksReducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state, action.payload];
+      return [...state, { ...action.payload }];
     case REMOVE_BOOK:
       return [...state.filter((book) => book.id !== action.payload.id)];
+    case UPDATE_BK:
+      return [...action.payload];
     default:
       return state;
   }
 };
 export default booksReducer;
+
+export const getItems = () => (dispatch) => fetch(`${API}`,
+  {
+    method: 'GET',
+    headers: { 'content-type': 'application/json' },
+  }).then((res) => res.json()).then((data) => {
+  const books = Object.keys(data).map((key) => {
+    const book = data[key][0];
+    return {
+      id: key,
+      ...book,
+    };
+  });
+  dispatch({ type: UPDATE_BK, payload: books });
+}).catch(() => {});
+
+export const addBook = (book) => (dispatch) => fetch(
+  `${API}`,
+  {
+    method: 'POST',
+    headers: { 'content-type': 'application/json' },
+    body: JSON.stringify({
+      item_id: book.id, title: book.title, author: book.author, category: book.category,
+    }),
+  },
+).then((res) => {
+  if (res.ok) {
+    dispatch({
+      type: ADD_BOOK,
+      payload: book,
+    });
+  }
+});
+
+export const removeBook = (bookId) => (dispatch) => {
+  const body = {
+    item_id: bookId,
+  };
+  return fetch(`${API}/${bookId}`,
+    {
+      method: 'DELETE',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(body),
+    }).then((res) => {
+    if (res.ok) {
+      dispatch({
+        type: REMOVE_BOOK,
+        id: bookId,
+      });
+    }
+  }).catch(() => {});
+};
--- src/Redux/Categories/Categories.js
+++ src/Redux/Categories/Categories.js
@@ -1,6 +1,6 @@
 const BOOK_STATUS = 'BOOK_STATUS';
 
-export const checkStatus = (status) => ({ type: BOOK_STATUS, payload: status });
+export const checkStatus = (id) => ({ type: BOOK_STATUS, payload: { id } });
 
 const initialState = {
   categories: [],
@@ -9,7 +9,7 @@ const initialState = {
 const categoriesReducer = (state = initialState, action = {}) => {
   switch (action.type) {
     case BOOK_STATUS:
-      return { ...state, status: 'Under construction' };
+      return 'Under construction';
     default:
       return state;
   }
